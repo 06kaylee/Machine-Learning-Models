0ab0bcebbd869ff366be99ef5c7f7221c74e8e17
06kaylee <06.kaylee@gmail.com>
2020-07-07 13:06:38 -0700

    initial commit

---
+++ Regression/multiple_linear_regression.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision = 2)
+results_arr = np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
+results_df = pd.DataFrame(data = results_arr, columns = ['Predicted', 'Actual'])
+print(results_df.head())
+
+print(r2_score(y_test, y_pred))
